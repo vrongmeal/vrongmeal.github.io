082ac2252270018d2d2a3880976a9d06555cb370
Vaibhav <vrongmeal@gmail.com>
2020-08-13 19:46:48 +0530

    feat: Add social icons and remove contact page.
    Signed-off-by: Vaibhav <vrongmeal@gmail.com>

--- src/pug.config.js
+++ src/pug.config.js
@@ -1,13 +1,9 @@
 module.exports = {
   locals: {
-    links: [
+    nav: [
       {
         title: "About",
         href:  "/about.pug",
-      },
-      {
-        title: "Contact",
-        href:  "/contact.pug",
       },
       {
         title: "Projects",
@@ -19,8 +15,40 @@ module.exports = {
       },
       {
         title: "Resume",
-        href:  "#resume"
-      }
+        href:  "#resume",
+      },
+    ],
+    contact: [
+      {
+        alt:  "Email (vrongmeal@gmail.com)",
+        icon: "/images/email.png",
+        href: "mailto:vrongmeal@gmail.com",
+      },
+      {
+        alt:  "Github (@vrongmeal)",
+        icon: "/images/github.png",
+        href: "https://github.com/vrongmeal",
+      },
+      {
+        alt:  "Twitter (@vrongmeal)",
+        icon: "/images/twitter.png",
+        href: "https://twitter.com/vrongmeal",
+      },
+      {
+        alt:  "LinkedIn (Vaibhav)",
+        icon: "/images/linkedin.png",
+        href: "https://www.linkedin.com/in/vrongmeal/",
+      },
+      {
+        alt:  "Facebook (Vaibhav VR)",
+        icon: "/images/facebook.png",
+        href: "https://www.facebook.com/vrongmeal",
+      },
+      {
+        alt:  "Instagram (@vrongmeal)",
+        icon: "/images/instagram.png",
+        href: "https://www.instagram.com/vrongmeal/",
+      },
     ]
   },
 };
